69f07a7196b9ef4cfc24c7028207396c6669660c
zhangyk <zhangyk@tju.edu.cn>
2019-02-28 19:30:09 +0800

    use args in Main parameter.ResetColor

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,12 +10,19 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            if (args.Length >= 2) { 
+              System.Console.WriteLine("Hello World!{0} is {1}", args[0], args[1]);
+            }
+            else
+            {
+                System.Console.WriteLine("Hello World! command line parameter does not meet the requirement!");
+            }
             Console.BackgroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Hello World!");
             System.Console.WriteLine("Hello World!");
             Console.ForegroundColor = ConsoleColor.Yellow;
             System.Console.WriteLine("Hello World!");
+            Console.ResetColor();
             
         }
     }
